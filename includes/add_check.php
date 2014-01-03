03577b8d99c1b6029b8c29a4a27d57b565893d61
Stephen Mareches <stephen.mareches@payscape.com>
2014-01-03 12:17:23 -0500

    Revert add_check.php to previous version
    add_check was committed with credit card version of Sale,
    reverted to December 21 version
    
    Signed-off-by: Stephen Mareches <stephen.mareches@payscape.com>

--- includes/add_check.php
+++ includes/add_check.php
@@ -1,28 +1,31 @@
 <?php 
 
-$posturl = 'https://secure.payscapegateway.com/api/transact.php';
-$order_id = 'TestCC';
 
-$visa = 4111111111111111;
-$mastercard = 5431111111111111;
-$discover = 6011601160116611;
-$american_express = 341111111111111;
-$cc_expire = '1025'; // 10/25
-$cvv = 123;
+	/* variables for Check Transactions */
 
-$key = '\!b2#Iwu%)4_tUdpAxO|GDWW?20:V.w';		// Replace with your Payscape Key
-$key_id = '449510';
-$type = 'sale';
-$time = gmdate('YmdHis');
+	$posturl = 'https://secure.payscapegateway.com/api/transact.php';
+	$order_id = 'Test';
+	$key = '\!b2#1wu%)4_tUdpAxO|GDWW?20:V.w';		// Replace with your Payscape Key
+	$key_id = '449510';
+	$type = 'sale';
+	$time = gmdate('YmdHis');
+	$order_id = 'TestCheck';
+	
+	$ipaddress = $_SERVER['REMOTE_ADDR'];
+	
+	$account_ach = '123123123'; // Replace with your Bank Account Number (ACH)
+	$routing_ach = '123123123'; // Replace with your Bank Routing Number (ACH)
+	$account_holder_type = 'business'; // Replace with your Payscape Account Holder Type (business / personal)
+	$account_type = 'checking'; // Replace with your bank account type (checking / savings)
+	$checkname = 'Test'; // Replace with the name on your ACH Account
 
-$ipaddress = $_SERVER['REMOTE_ADDR'];
-
-$orderid = date('YmdHis') . "Test";
-
-			require_once 'classes/Payscape/Payscape.php';
+	$orderid = date('YmdHis') . "TestCheck";	
+	
+	require_once 'classes/Payscape/Payscape.php';
 	
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
 				
 		/* triggers */
 		
@@ -35,68 +38,51 @@ $orderid = date('YmdHis') . "Test";
 		* */
 		
 		
-			/*
-			$data_debug = $_POST;	
-		
-				echo "<pre>";
-					print_r($_POST);
-				echo "</pre>";	
-				exit();
-			*/
-	$amount = $_POST['amount'];
-	$payment = 'credit card';
-	$ccnumber = $_POST['ccnumber'];
-	$ccexp = $_POST['ccexp'];
 
-	$cvv = $_POST['cvv'];
+    	$amount = $_POST['amount'];
+    	$payment = 'check';
+    	$checkname = $_POST['checkname'];
+    	$checkaba = $_POST['checkaba'];
+    	$checkaccount = $_POST['checkaccount'];
+    	$account_holder_type = $_POST['account_holder_type'];
+    	$account_type = $_POST['account_type'];
+    	$sec_code = 'WEB';
+    	
+    	$firstname = $_POST['firstname'];
+    	$lastname = $_POST['lastname'];
+    	$company = $_POST['company'];
+    	$address1 = $_POST['address1'];
+    	$city = $_POST['city'];
+    	$state = $_POST['state'];
+    	$zip = $_POST['zip'];
+    	$country = $_POST['country'];
+    	$phone = $_POST['phone'];
+    	$fax = $_POST['fax'];
+    	$email = $_POST['email'];
+    	
+    	$time = gmdate('YmdHis');
+    	$hash = md5($order_id|$amount|$time|$key);
+    	
 	
-	$firstname = $_POST['firstname'];
-	$lastname = $_POST['lastname'];
-	$company = $_POST['company'];
-	$address1 = $_POST['address1'];
-	$city = $_POST['city'];
-	$state = $_POST['state'];
-	$zip = $_POST['zip'];
-	$country = $_POST['country'];
-	$phone = $_POST['phone'];
-	$fax = $_POST['fax'];
-	$email = $_POST['email'];
-	
-	$time = gmdate('YmdHis');	
-	$hash = md5($order_id|$amount|$time|$key);
-	
-	
-/*	
-	echo "<br>ORDER ID: $order_id";
-	echo "<br>AMOUNT: $amount";
-	echo "<br>TIME: $time";
-	echo "<br>KEY: $key";
-	echo "<br>MD5(order_id|amount|time|): " . md5("$order_id|$amount|$time|$key") . "<br>";
 
-
-	echo "<br>md5(order_id|amount|time): " . md5("$order_id|$amount|time") . "<br>";
-	
-	
-	
-	echo "<br>HASH: $order_id|$amount|$time|$key";
-	echo "<br>PREPARED HASH: $hash";
-*/			
+    	
+    	$hash = md5($order_id|$amount|$time|$key);
+			
 		$incoming = array();
-		$incoming['type'] = "$type";
-		$incoming['amount'] = $amount;
-		$incoming['payment'] = 'credit card';
-		
-//		$incoming['key_id'] = $key_id;
-//		$incoming['hash'] = $hash;
-//		$incoming['time'] = $time;
+		$incoming['type'] = $type;
+		$incoming['key_id'] = $key_id;
 
-		$incoming['ccnumber'] = $ccnumber;
-		$incoming['ccexp'] = $ccexp;
-		$incoming['cvv'] = $cvv;
-		
+		$incoming['time'] = $time;
+		$incoming['checkname'] = $checkname;
+		$incoming['checkaba'] = $checkaba;
+		$incoming['checkaccount'] = $checkaccount;
+		$incoming['account_holder_type'] = $account_holder_type;
+		$incoming['account_type'] = $account_type;					
+		$incoming['amount'] = $amount;			
+		$incoming['payment'] = 'check';
 		$incoming['firstname'] = $firstname;
 		$incoming['lastname'] = $lastname;
-		$incoming['company'] = $company;
+		$incoming['company'] = $company;		
 		$incoming['address1'] = $address1;
 		$incoming['city'] = $city;
 		$incoming['state'] = $state;
@@ -105,89 +91,70 @@ $orderid = date('YmdHis') . "Test";
 		$incoming['phone'] = $phone;
 		$incoming['fax'] = $fax;
 		$incoming['email'] = $email;
-		$incoming['orderid'] = $orderid;
-
-
-	
+		$incoming['orderid'] = $orderid;		
 		
-
 		$Payscape = NEW Payscape();
-		$response = $Payscape->Sale($incoming);
-		
-		
-		/*
-		 echo "<pre>";
-		echo "INCOMING: <br>";
-		print_r($incoming);
-		*/
+		$response = $Payscape->SaleCheck($incoming);
 		
 		/*
-		echo "<br>RESPONSE:<br>";
-		print_r($response);
 		echo "<pre>";
-		*/
-		//exit();
+		echo "INCOMING: ";
+		print_r($response);
+		echo "</pre>";
 		
+		
+		echo $response;
+		exit();
+		*/
+
 		parse_str($response, $result_array);
 		
-		/*
-		echo "<pre>";
-		echo "RESULT ARRAY: ";
-		print_r($result_array);
-		echo "</pre>";
-		*/
-	//	exit();
-		
-		if($result_array['response']==1){
-		
-			$transactionid = $result_array['transactionid'];		
-			$message = "The transaction was successful "; 
-		
-		
-		/* save the submission and transaction details */
-			
-		$sql = "INSERT INTO `transactions` (`type`, `key_id`, 
-				`hash`, `time`, `ccnumber`, `ccexp`,  
-				`amount`, `cvv`, `payment`, `ipaddress`, `firstname`, 
-				`lastname`, `company`, `address1`, `city`, `state`, `zip`, `country`, 
-				`phone`, `fax`, `email`, `orderid`, `transactionid`) 
-				VALUES('$type', '$key_id',
-				'$hash', '$time', '$ccnumber', '$ccexp', 
-				$amount, '$cvv', '$payment', '$ipaddress', '$firstname', 
-				'$lastname', '$company', '$address1', '$city', '$state', '$zip', '$country',
-				'$phone', '$fax', '$email', '$orderid', $transactionid)";
-						/*		
-								echo "SQL: <BR>";
-										echo "<pre>";
-								echo $sql;
-										echo "</pre>";
-										
-								exit();		
-						*/
-					if(!mysqli_query($conn, $sql)){
-						/* for testing */
-						printf("Error: %s\n", mysqli_error($conn));
-						$message .= " but could not be saved to the database";
-					} else {
-						$message .= " and has been Saved to the database.";
-					}
-	
-			
-			} else {
-				$message = "Transaction has failed.";
-			}		
-			
-			
-			
-			mysqli_close($conn);
-									
+					if($result_array['response']==1){
+						
+						$message = "The transaction was successful";
+						$transactionid = $result_array['transactionid'];
+						
+					/* save the submission */
+						
+					$sql = "INSERT INTO `transactions` (`type`, `key_id`, 
+							`hash`, `time`, `checkname`, `checkaba`, 
+							`checkaccount`, `account_holder_type`, `account_type`, `sec_code`, 
+							`amount`, `payment`, `ipaddress`, `firstname`, 
+							`lastname`, `company`, `address1`, `city`, `state`, `zip`, `country`, 
+							`phone`, `fax`, `email`, `orderid`, `transactionid`) 
+							VALUES('$type', '$key_id',
+							'$hash', '$time', '$checkname', '$checkaba', 
+							'$checkaccount', '$account_holder_type', '$account_type', '$sec_code',
+							$amount, '$payment', '$ipaddress', '$firstname', 
+							'$lastname', '$company', '$address1', '$city', '$state', '$zip', '$country',
+							'$phone', '$fax', '$email', '$orderid', $transactionid)";
+					
+			//	echo "$sql";
+			//	exit();	
+					
+					
+								if(!mysqli_query($conn, $sql)){
+									printf("Error: %s\n", mysqli_error($conn));
+						
+									$message .= " but could not be saved to the database";
+								} else {
+						
+									$message .= "and has been saved to the database.";
+								}	
 
-    } else {
-    	
-    	require_once 'includes/add_cc_form.php';
-    	
-    }		
- //   echo $message;
-    
- // end add_cc   
+							
+							} else {
+								$message = "Transaction has failed.";
+							}
+								
+							mysqli_close($conn);
+		
+				
+			} else {
+		    	require_once 'includes/add_cc_form.php';
+    		}// method post	
+
+    	//	echo $message;
+    		
+// end add_check		
 ?>		
